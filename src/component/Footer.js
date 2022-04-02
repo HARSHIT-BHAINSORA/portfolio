cc3231e79a15e9ec6a26adecaa6dea9bdaafc909
xarck <iamashkin@gmail.com>
2022-04-02 20:36:21 +0530

    footer problem solved

--- src/component/Footer.js
+++ src/component/Footer.js
@@ -2,35 +2,51 @@ import React from "react";
 import "./Footer.css";
 
 function Footer() {
-  return (
-    <div className="footer">
-      <div className="footer_container">
-        <div className="footer_logo">
-          <img src={require("./image/logo_image.png")} alt="" />
-        </div>
-        <div className="hit_line">
-          <h4>Living ,learning & leveling up one day at a time.</h4>
-        </div>
-        <div className="soical_media">
-          <button>
-            <a href="https://www.google.com/" target={"_blank"}>
-              hi
-            </a>
-          </button>
+    return (
+        <div className="footer">
+            <div className="box">
+                <div className="headings">
+                    <h1 class="title">Start a project</h1>
+                </div>
+                <div className="data">
+                    <p className="para">
+                        Intersted in working together? We should queue up a chat
+                        . I'll buy the coffee.
+                    </p>
+                </div>
+                <div className="btn">
+                    <button className="btn_inside">
+                        <span>Let's Connect</span>
+                    </button>
+                </div>
+            </div>
+            <div className="footer_container">
+                <div className="footer_logo">
+                    <img src={require("./image/logo_image.png")} alt="" />
+                </div>
+                <div className="hit_line">
+                    <h4>Living ,learning & leveling up one day at a time.</h4>
+                </div>
+                <div className="soical_media">
+                    <button>
+                        <a href="https://www.google.com/" target={"_blank"}>
+                            hi
+                        </a>
+                    </button>
 
-          <button className="btnn"></button>
-          <button className="btnn"></button>
-          <button className="btnn"></button>
+                    <button className="btnn"></button>
+                    <button className="btnn"></button>
+                    <button className="btnn"></button>
+                </div>
+                <div className="creator">
+                    <p>
+                        Handcraft by me <span>©</span>
+                        Harbazzar.
+                    </p>
+                </div>
+            </div>
         </div>
-        <div className="creator">
-          <p>
-            Handcraft by me <span>©</span>
-            Harbazzar.
-          </p>
-        </div>
-      </div>
-    </div>
-  );
+    );
 }
 
 export default Footer;
--- src/component/Home.js
+++ src/component/Home.js
@@ -5,86 +5,68 @@ import features from "./Feature";
 import Footer from "./Footer";
 
 function createSkill(feature) {
-  return (
-    <Skill
-      title={feature.title}
-      image={feature.image}
-      about={feature.about}
-      head={feature.head}
-      property={feature.property}
-      toolhead={feature.toolhead}
-      tools={feature.tools}
-    />
-  );
+    return (
+        <Skill
+            title={feature.title}
+            image={feature.image}
+            about={feature.about}
+            head={feature.head}
+            property={feature.property}
+            toolhead={feature.toolhead}
+            tools={feature.tools}
+        />
+    );
 }
 
 function Home() {
-  return (
-    <div className="home">
-      <div className="container">
-        <div className="head__line">
-          <h1>Coder , Web Developer & Student</h1>
-        </div>
-        <div className="tag__line">
-          <p>
-            The computer was born to solve problems that did not exist before.
-          </p>
-        </div>
-        <div className="avatar">
-          <img
-            className="avatar__image"
-            src={require("./image/avatar_image.png")}
-            alt=""
-          ></img>
-        </div>
-        <div className="computer">
-          <img
-            className="computer__image"
-            src="https://mattfarley.ca/img/hero.svg"
-          />
-        </div>
-      </div>
+    return (
+        <div className="home">
+            <div className="container">
+                <div className="head__line">
+                    <h1>Coder , Web Developer & Student</h1>
+                </div>
+                <div className="tag__line">
+                    <p>
+                        The computer was born to solve problems that did not
+                        exist before.
+                    </p>
+                </div>
+                <div className="avatar">
+                    <img
+                        className="avatar__image"
+                        src={require("./image/avatar_image.png")}
+                        alt=""
+                    ></img>
+                </div>
+                <div className="computer">
+                    <img
+                        className="computer__image"
+                        src="https://mattfarley.ca/img/hero.svg"
+                    />
+                </div>
+            </div>
 
-      <div className="banner">
-        <div className="head">
-          <h2> Hi, I'm Harshit. Nice to meet you </h2>
+            <div className="banner">
+                <div className="head">
+                    <h2> Hi, I'm Harshit. Nice to meet you </h2>
+                </div>
+                <div className="description">
+                    <p>
+                        Since , I started my journey in Web Dev from my frist
+                        year of my college ,I have enough knowledge about
+                        front-end langauages currently work with react.js
+                        framwork. mine upcoming target is to learn more about
+                        back-end . and eargly willing to work as a freelancer
+                        for business and comsumers users.
+                    </p>
+                </div>
+            </div>
+            <div className="work">
+                <div className="skills">{features.map(createSkill)}</div>
+            </div>
+            <Footer />
         </div>
-        <div className="description">
-          <p>
-            Since , I started my journey in Web Dev from my frist year of my
-            college ,I have enough knowledge about front-end langauages
-            currently work with react.js framwork. mine upcoming target is to
-            learn more about back-end . and eargly willing to work as a
-            freelancer for business and comsumers users.
-          </p>
-        </div>
-      </div>
-      <div className="work">
-        <div className="skills">{features.map(createSkill)}</div>
-      </div>
-
-      <div className="let_start">
-        <div className="box">
-          <div className="headings">
-            <h1 class="title">Start a project</h1>
-          </div>
-          <div className="data">
-            <p className="para">
-              Intersted in working together? We should queue up a chat . I'll
-              buy the coffee.
-            </p>
-          </div>
-          <div className="btn">
-            <button className="btn_inside">
-              <span>Let's Connect</span>
-            </button>
-          </div>
-        </div>
-      </div>
-
-      <Footer />
-    </div>
-  );
+    );
 }
 
 export default Home;
