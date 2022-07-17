6841a6de1c912f86c3b95d0785f5151fd5aa3d0d
HARSHIT <bharshit468@gamail.com>
2022-07-17 18:25:28 +0530

    Light and Drak mode Implemented

--- src/component/App.js
+++ src/component/App.js
@@ -1,30 +1,52 @@
-import React from "react";
+import React, { useState } from "react";
 import "./App.css";
 import Header from "./Header";
 import Home from "./Home";
 import Project from "./Project";
 import Contact from "./Contact";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import { createContext } from "react";
+import ReactSwitch from "react-switch";
+
+export const ThemeContext = createContext(null);
 
 function App() {
+  const [theme, setTheme] = useState("light");
+
+  const toggleTheme = () => {
+    setTheme((curr) => (curr === "light" ? "dark" : "light"));
+  };
+
   return (
     <Router>
-      <div>
-        <Header />
-        <Switch>
-          <Route path="/project">
-            <Project />
-          </Route>
+      <ThemeContext.Provider value={{ theme, toggleTheme }}>
+        <div className="App" id={theme}>
+          <Header />
+          <div className="switch">
+            <ReactSwitch
+              className="tbtn"
+              onChange={toggleTheme}
+              checked={theme === "dark"}
+            />
+            <label className="lable">
+              {theme === "light" ? "Light Mode" : "Dark Mode"}
+            </label>
+          </div>
+          <Switch>
+            <Route path="/project">
+              <Project />
+            </Route>
 
-          <Route path="/contact">
-            <Contact />
-          </Route>
+            <Route path="/contact">
+              <Contact />
+            </Route>
 
-          <Route path="/">
-            <Home />
-          </Route>
-        </Switch>
-      </div>
+            <Route path="/">
+              <Home />
+            </Route>
+          </Switch>
+        </div>
+      </ThemeContext.Provider>
     </Router>
   );
 }
--- src/component/Home.js
+++ src/component/Home.js
@@ -5,68 +5,66 @@ import features from "./Feature";
 import Footer from "./Footer";
 
 function createSkill(feature) {
-    return (
-        <Skill
-            title={feature.title}
-            image={feature.image}
-            about={feature.about}
-            head={feature.head}
-            property={feature.property}
-            toolhead={feature.toolhead}
-            tools={feature.tools}
-        />
-    );
+  return (
+    <Skill
+      title={feature.title}
+      image={feature.image}
+      about={feature.about}
+      head={feature.head}
+      property={feature.property}
+      toolhead={feature.toolhead}
+      tools={feature.tools}
+    />
+  );
 }
 
 function Home() {
-    return (
-        <div className="home">
-            <div className="container">
-                <div className="head__line">
-                    <h1>Coder , Web Developer & Student</h1>
-                </div>
-                <div className="tag__line">
-                    <p>
-                        The computer was born to solve problems that did not
-                        exist before.
-                    </p>
-                </div>
-                <div className="avatar">
-                    <img
-                        className="avatar__image"
-                        src={require("./image/avatar_image.png")}
-                        alt=""
-                    ></img>
-                </div>
-                <div className="computer">
-                    <img
-                        className="computer__image"
-                        src="https://mattfarley.ca/img/hero.svg"
-                    />
-                </div>
-            </div>
-
-            <div className="banner">
-                <div className="head">
-                    <h2> Hi, I'm Harshit. Nice to meet you </h2>
-                </div>
-                <div className="description">
-                    <p>
-                        Since , I started my journey in Web Dev from my frist
-                        year of my college ,I have enough knowledge about
-                        front-end langauages currently work with react.js
-                        framwork. mine upcoming target is to learn more about
-                        back-end . and eargly willing to work as a freelancer
-                        for business and comsumers users.
-                    </p>
-                </div>
-            </div>
-            <div className="work">
-                <div className="skills">{features.map(createSkill)}</div>
-            </div>
-            <Footer />
+  return (
+    <div className="home">
+      <div className="container">
+        <div className="head__line">
+          <h1>Coder , Web Developer & Student</h1>
         </div>
-    );
+        <div className="tag__line">
+          <p>
+            The computer was born to solve problems that did not exist before.
+          </p>
+        </div>
+        <div className="avatar">
+          <img
+            className="avatar__image"
+            src={require("./image/avatar_image.png")}
+            alt=""
+          ></img>
+        </div>
+        <div className="computer">
+          <img
+            className="computer__image"
+            src="https://mattfarley.ca/img/hero.svg"
+          />
+        </div>
+      </div>
+
+      <div className="banner">
+        <div className="head">
+          <h2> Hi, I'm Harshit. Nice to meet you </h2>
+        </div>
+        <div className="description">
+          <p>
+            Since , I started my journey in Web Dev from my frist year of my
+            college ,I have enough knowledge about front-end langauages
+            currently work with react.js framework. mine upcoming target is to
+            learn more about back-end . and eargly willing to work as a
+            freelancer for business and comsumers users.
+          </p>
+        </div>
+      </div>
+      <div className="work">
+        <div className="skills">{features.map(createSkill)}</div>
+      </div>
+      <Footer />
+    </div>
+  );
 }
 
 export default Home;
